d416b40e4384e8f7e1e154338f084f500d7b9205
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-16 18:38:17 -0400

    Added showBalance

--- js/app.js
+++ js/app.js
@@ -38,7 +38,24 @@ class BudgetApp {
   }
   // show balance
   showBalance() {
-    console.log('Hi this')
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed')
+    } else if (total > 0) {
+      this.balance.classList.remove('showRed', 'showBlack');
+      this.balance.classList.add('showGreen')
+    } else if (total === 0) {
+      this.balance.classList.remove('showRed', 'showGreen');
+      this.balance.classList.add('shoeBlack')
+    }
+  }
+  // total expense
+  totalExpense() {
+    let total = 400;
+    return total
   }
 }
 
