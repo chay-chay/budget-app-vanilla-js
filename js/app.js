fcf956912f68bee53d1c2a8fb4d27a265d24b023
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-16 13:17:53 -0400

    Added eventListeners & submitForm

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-class UI {
+class BudgetApp {
   constructor() {
     this.budgetFeedback = document.querySelector(".budget-feedback");
     this.expenseFeedback = document.querySelector(".expense-feedback");
@@ -15,4 +15,39 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  submitBudgetForm() {
+    console.log("hi")
+    // const value = this.budgetInput.value;
+    // if (value === "" || value < 0) {
+    //   this.budgetFeedback.classList.add("showItem");
+    //   this.budgetFeedback.innerHTML = `<p>Value cannot be empty or negative</p>`;
+    // }
+  }
 }
+
+function eventListeners() {
+  const budgetForm = document.getElementById("budget-form");
+  const expenseForm = document.getElementById("expense-form");
+  const expenseList = document.getElementById("expense-list");
+
+  // new instance of budgetApp class
+  const budget = new BudgetApp();
+
+  // budget form submit
+  budgetForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    budget.submitBudgetForm();
+  });
+  // expense form submit
+  expenseForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+  // expense click
+  expenseList.addEventListener("click", function (event) {
+    
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  eventListeners();
+});
