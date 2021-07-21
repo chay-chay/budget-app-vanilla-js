14b10abbbd0f6dc80527b52c76d25b809936f8ef
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-07-21 00:21:04 -0400

    Added delete function

--- js/app.js
+++ js/app.js
@@ -124,7 +124,7 @@ class BudgetApp {
     let parent = element.parentElement.parentElement.parentElement;
     // remove from DOM 
     this.expenseList.removeChild(parent)
-
+    // remove from the List
     let expense = this.itemList.filter(function (item) {
       return item.id === id
     })
@@ -140,7 +140,16 @@ class BudgetApp {
   }
   // delete expense
   deleteExpense(element) {
-    
+    let id = parseInt(element.dataset.id);
+    let parent = element.parentElement.parentElement.parentElement;
+    // remove from DOM 
+    this.expenseList.removeChild(parent)
+    // remove from the List
+    let tempList = this.itemList.filter(function (item) {
+      return item.id !== id;
+    })
+    this.itemList = tempList;
+    this.showBalance()
   }
 }
 
